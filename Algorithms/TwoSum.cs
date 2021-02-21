deddaa6b439d94ca633b1f5f0e42fff11ef1c2ae
guodan <guodancoder@gmail.com>
2021-02-22 00:29:41 +0800

    two sum add dictionary, but still slowly

--- Algorithms/TwoSum.cs
+++ Algorithms/TwoSum.cs
@@ -1,4 +1,6 @@
-public class SolutionTwoSum {
+using System.Collections.Generic;
+using System;
+public class SolutionTwoSum {//224ms
     public int[] TwoSum(int[] nums, int target) {
         for(int i = 0; i < nums.Length; i++){
                 for(int j = i + 1; j< nums.Length; j++){
@@ -7,6 +9,20 @@ public class SolutionTwoSum {
                     }
                 }
             }
+        return null;
+    }
+    public int[] TwoSum_Dictionary(int[] nums, int target) {//240ms
+        Dictionary<int, int> numDictionary = new Dictionary<int, int>();
+        for(int i = 0; i < nums.Length; i++){
+            int temp = target - nums[i];
+            int index = 0;
+            if(numDictionary.TryGetValue(temp, out index)){
+                return new int[]{i, index};
+            }
+            if(!numDictionary.ContainsKey(nums[i])){
+                numDictionary.Add(nums[i], i);
+            }
+        }
         return null;
     }
 }
